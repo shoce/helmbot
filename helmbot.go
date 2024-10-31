b8c78e82bcb0190cc49dcfb59e083c0bf8c7dd6b
shoce <yogze@icloud.com>
2024-11-01 00:58:14 +0530

    log serverconfigs

--- helmbot.go
+++ helmbot.go
@@ -590,6 +590,9 @@ func ServerPackagesUpgrade() (err error) {
 		log("WARNING GetValuesFile `%s`: %v", PackagesLocalPath, err)
 	}
 
+	log("serverconfigs: %#v", serverconfigs)
+	log("serverconfigs: %+v", serverconfigs)
+
 	return nil
 
 	err = GetValues("helm-packages.values.yaml", nil, &serverconfigs)
