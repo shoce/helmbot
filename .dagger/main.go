9518b233409e9cbaef407b761d0578c28a1c5d22
shoce <yogze@icloud.com>
2025-02-06 23:07:56 +0530

    dagger errgroup

--- .dagger/main.go
+++ .dagger/main.go
@@ -4,13 +4,28 @@ import (
 	"context"
 
 	"dagger/helmbot/internal/dagger"
+
+	"golang.org/x/sync/errgroup"
 )
 
 const (
+	GolangDockerImage = "golang:1.23.6"
+	AlpineDockerImage = "alpine:3.21.2"
+	ExposedPort       = 80
+
 	NL = "\n"
 )
 
 var (
+	Platforms = []dagger.Platform{
+		"linux/amd64",
+		"linux/arm64",
+	}
+	SourceFiles = []string{
+		"helmbot.go", "drlatest.go", "minio.go", "tg.go",
+		"go.mod", "go.sum",
+	}
+
 	Ctx = context.TODO()
 )
 
@@ -21,54 +36,44 @@ func (m *Helmbot) Build(
 	srcdir *dagger.Directory,
 ) []*dagger.Container {
 
-	var Platforms = []dagger.Platform{
-		"linux/amd64",
-		"linux/arm64",
-	}
 	c := make([]*dagger.Container, 0, len(Platforms))
 
-	fnn := []string{
-		"helmbot.go", "drlatest.go", "minio.go", "tg.go",
-		"go.mod", "go.sum",
-	}
 	ff := []*dagger.File{}
-	for _, fn := range fnn {
+	for _, fn := range SourceFiles {
 		ff = append(ff, srcdir.File(fn))
 	}
 
+	eg, _ := errgroup.WithContext(Ctx)
+
 	for _, platform := range Platforms {
 
-		/*
-			arch, err := dag.Containerd().ArchitectureOf(Ctx, platform)
-			if err != nil {
-				return nil
-			}
-		*/
+		eg.Go(func() (err error) {
 
-		// https://hub.docker.com/_/golang/tags/
-		a := dag.Container(dagger.ContainerOpts{Platform: platform}).
-			From("golang:1.23.6").
-			WithFiles("/root/helmbot/", ff).
-			WithWorkdir("/root/helmbot/").
-			WithEnvVariable("CGO_ENABLED", "0").
-			WithExec([]string{"go", "get", "-v"}).
-			WithExec([]string{"go", "build", "-o", "helmbot", "."})
+			// https://hub.docker.com/_/golang/tags/
+			a := dag.Container(dagger.ContainerOpts{Platform: platform}).
+				From(GolangDockerImage).
+				WithFiles("/root/helmbot/", ff).
+				WithWorkdir("/root/helmbot/").
+				WithEnvVariable("CGO_ENABLED", "0").
+				WithExec([]string{"go", "get", "-v"}).
+				WithExec([]string{"go", "build", "-o", "helmbot", "."})
 
-		// https://hub.docker.com/_/alpine/tags/
-		b := dag.Container(dagger.ContainerOpts{Platform: platform}).
-			From("alpine:3.21.2").
-			WithExec([]string{"apk", "add", "--no-cache", "gcompat"}).
-			WithExec([]string{"ln", "-s", "-f", "-v", "ld-linux-x86-64.so.2", "/lib/libresolv.so.2"}).
-			WithFile("/bin/helmbot", a.File("/root/helmbot/helmbot")).
-			WithWorkdir("/root/").
-			WithEntrypoint([]string{"/bin/helmbot"}).
-			WithExposedPort(80)
+			// https://hub.docker.com/_/alpine/tags/
+			b := dag.Container(dagger.ContainerOpts{Platform: platform}).
+				From(AlpineDockerImage).
+				WithExec([]string{"apk", "add", "--no-cache", "gcompat"}).
+				WithExec([]string{"ln", "-s", "-f", "-v", "ld-linux-x86-64.so.2", "/lib/libresolv.so.2"}).
+				WithFile("/bin/helmbot", a.File("/root/helmbot/helmbot")).
+				WithWorkdir("/root/").
+				WithEntrypoint([]string{"/bin/helmbot"}).
+				WithExposedPort(ExposedPort)
 
-		/*
-			p, _ := b.Platform(Ctx)
-			fmt.Printf("platform==%s arch==%s"+NL, p, arch)
-		*/
-		c = append(c, b)
+			c = append(c, b)
+
+			return err
+		})
+
+		eg.Wait()
 
 	}
 
