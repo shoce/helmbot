3115fa65c9912f5e0b8fd9d7de6a352e770729de
shoce <yogze@icloud.com>
2025-02-11 02:26:19 +0530

    until next update

--- helmbot.go
+++ helmbot.go
@@ -598,7 +598,7 @@ func ServerPackagesUpdate() (err error) {
 		log("DEBUG packages --- Name==%s AlwaysForceNow==%v AllowedHours==%v TimeNowHour==%v UpdateInterval==%v UpdateDelay==%v UpdateTimestamp=%v", p.Name, *p.AlwaysForceNow, p.AllowedHoursList, timenowhour, p.UpdateIntervalDuration, p.UpdateDelayDuration, p.UpdateTimestamp.Format("060102:150405-0700"))
 
 		if d := time.Now().Sub(p.UpdateTimestamp).Truncate(time.Second); d < p.UpdateIntervalDuration {
-			log("DEBUG packages "+SPAC+"%v since update < %v UpdateInterval -- skipping update", d, p.UpdateIntervalDuration)
+			log("DEBUG packages "+SPAC+"%v until next update", p.UpdateIntervalDuration-d)
 			continue
 		}
 
