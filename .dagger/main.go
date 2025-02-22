46a52dc8bce96274e249a26ce7fa314190bf7453
shoce <yogze@icloud.com>
2025-02-22 15:17:24 +0530

    VERSION

--- .dagger/main.go
+++ .dagger/main.go
@@ -64,7 +64,7 @@ func (m *Helmbot) Build(
 				WithEnvVariable("CGO_ENABLED", "0").
 				WithEnvVariable("GOARCH", arch).
 				WithExec([]string{"go", "get", "-v"}).
-				WithExec([]string{"go", "build", "-ldflags", "-X main.VERSION=" + os.Getenv("VERSION"), "-o", "helmbot", "."})
+				WithExec([]string{"go", "build", "-o", "helmbot", "-ldflags", "-X main.VERSION=" + os.Getenv("VERSION"), "."})
 
 			// https://hub.docker.com/_/alpine/tags/
 			b := dag.Container(dagger.ContainerOpts{Platform: platform}).
